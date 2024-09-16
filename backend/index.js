4da4aca247ad89d20ba3c59f4d74eeb56a62f5c4
Mohamed Tarek <mohammed.tarek.rezk@gmail.com>
2024-09-16 21:00:52 +0300

    last

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,13 @@ app.use(express.urlencoded({ extended: true}))
 app.use(cookieParser())
 app.use("/api/users", userRoute)
  __dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+// 	// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
 
 
 app.use((error , req , res , next)=>{
